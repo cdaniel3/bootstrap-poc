e4f14ff91b70b174cb3f313584bab7e9315cbed3
Cory Daniel <cdaniel3@gmail.com>
2017-10-21 23:01:21 -0500

    modified sidenav

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/bootstrap', function(req, res) {
-	res.send('Hellow wolrd')
-})
-
 app.listen(3000, function() {
 	console.log('example app listening on port 3000')
+})
+
+app.get('/bootstrap', function(req, res) {
+	res.send('Hellow wolrd')
 })
 
 app.use('/bootstrap', express.static('public'))
